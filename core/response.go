83b37a20b9f2cb705c590885193d15a3b8574c83
adi <aditya03joshi@gmail.com>
2026-04-12 15:03:20 +0530

    int64 response

--- core/response.go
+++ core/response.go
@@ -19,3 +19,15 @@ func readSimpleString(data []byte) (string, int, error) {
 func readError(data []byte) (string, int, error) {
 	return readSimpleString(data)
 }
+
+func readInt64(data []byte) (int64, int, error) {
+	pos := 1
+
+	var val int64 = 0
+
+	for ; data[pos] != '\r'; pos++ {
+		val = val*10 + int64(data[pos]-'0')
+	}
+
+	return val, pos + 2, nil
+}
