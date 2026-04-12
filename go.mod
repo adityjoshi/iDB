93c2330d1c4e315d452579db23184bde898ac69e
adi <aditya03joshi@gmail.com>
2026-04-12 10:53:55 +0530

    configs added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adityjoshi/iDB
+
+go 1.25.5
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
 import (
-"fmt"
-"encoding/json"
+	"flag"
+	"log"
 )
 
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for iDB")
+	flag.IntVar(&config.Port, "Port", 1926, "port for thr iDB")
+	flag.Parse()
+}
+
+func main() {
+	setupFlags()
+	log.Println(`
+	_____ _____ ____  
+  |_   _|  __ \  _ \ 
+    | | | |  | | |_) |
+    | | | |  | |  _ < 
+   _| |_| |__| | |_) |
+  |_____|_____/|____/ 
+	
+`)
+	log.Println("If Steve Jobs ever built a database, he’d call it iDB. iDB started.")
+
+}
