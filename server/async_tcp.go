a8785f42b5634c941cd53a72f7225439fb85932a
adi <aditya03joshi@gmail.com>
2026-04-19 17:17:05 +0530

    async tcp

--- server/async_tcp.go
+++ server/async_tcp.go
@@ -1,3 +1,13 @@
 package server
 
+import (
+	"log"
+
+	"github.com/adityjoshi/iDB/config"
+)
+
 var connected_clients int = 0
+
+func AsyncTcpServer() error {
+	log.Println("Asysn Tcp Started on", config.Host, config.Port)
+}
