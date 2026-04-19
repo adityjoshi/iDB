5b65872121299ea496c6d271d48bce35e7bdeaae
adi <aditya03joshi@gmail.com>
2026-04-19 17:26:55 +0530

    async tcp

--- server/async_tcp.go
+++ server/async_tcp.go
@@ -15,4 +15,11 @@ func AsyncTcpServer() error {
 	var max_clients int = 20000
 
 	var events []syscall.Kevent_t = make([]syscall.Kevent_t, max_clients)
+
+	serverFD, err := syscall.Socket(syscall.AF_INET, syscall.O_NONBLOCK|syscall.SOCK_STREAM, 0)
+	if err != nil {
+		return err
+	}
+
+	defer syscall.Close(serverFD)
 }
