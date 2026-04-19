fd30d3b6a8a3bf351ad730b278ea3e78e1987d8b
adi <aditya03joshi@gmail.com>
2026-04-19 17:24:33 +0530

    async tcp

--- server/async_tcp.go
+++ server/async_tcp.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"log"
+	"syscall"
 
 	"github.com/adityjoshi/iDB/config"
 )
@@ -10,4 +11,8 @@ var connected_clients int = 0
 
 func AsyncTcpServer() error {
 	log.Println("Asysn Tcp Started on", config.Host, config.Port)
+
+	var max_clients int = 20000
+
+	var events []syscall.Kevent_t = make([]syscall.Kevent_t, max_clients)
 }
