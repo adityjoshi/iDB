8883fccfc5065d408cd8fec10b9e0091d3653f05
adi <aditya03joshi@gmail.com>
2026-04-19 16:51:10 +0530

    file descriptor

--- core/communication.go
+++ core/communication.go
@@ -9,3 +9,7 @@ type FileDescriptor struct {
 func (f *FileDescriptor) Write(b []byte) (int, error) {
 	return syscall.Write(f.FD, b)
 }
+
+func (f *FileDescriptor) Read(b []byte) (int, error) {
+	return syscall.Read(f.FD, b)
+}
