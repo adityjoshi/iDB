80ce616f8f4664fafef0efe07c32561cd3f18e61
adi <aditya03joshi@gmail.com>
2026-05-10 20:17:29 +0530

    aof

--- core/aof.go
+++ core/aof.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/adityjoshi/iDB/config"
 )
+
+func dumpKey(fp *os.File, key string, obj *Object) {
+	cmd := fmt.Sprintf("SET %s %s", key, obj.value)
+	tokens := strings.Split(cmd, " ")
+	fp.Write(Encode(tokens, false))
+}
 
 func DUMPALLAOF() {
 	fp, err := os.OpenFile(config.AOFFILE, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
